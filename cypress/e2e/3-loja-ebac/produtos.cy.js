3b7ab6d57779114af162496ff3bb3826b6e6fbab
jessepr372 <105854432+jessepr372@users.noreply.github.com>
2024-11-07 21:55:08 -0300

    produtos

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+    
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block')
+            //.first()
+            //.last()
+            .contains('Atomic Endurance Running Tee (Crew-Neck)')
+            .click()
+
+            cy.get('#tab-title-description > a').should('contain', 'Descrição')
+        
+    });
+});
